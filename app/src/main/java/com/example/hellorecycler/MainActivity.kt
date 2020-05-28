f3cb14787fb7a584575876de81acf9ea1113a65a
Andrew Gray <agray@captechconsulting.com>
2020-05-28 15:35:17 -0400

    add variables

--- app/src/main/java/com/example/hellorecycler/MainActivity.kt
+++ app/src/main/java/com/example/hellorecycler/MainActivity.kt
@@ -2,8 +2,13 @@ package com.example.hellorecycler
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
 
     // test commit
     override fun onCreate(savedInstanceState: Bundle?) {
